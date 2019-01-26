f078c9330bff60d05293a15a9228668104d8b043
William Santos <william.santos88@gmail.com>
2019-01-26 12:18:49 -0200

    Endpoint /clientes/{id} disponível

---
+++ src/main/java/com/wsbdac/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.wsbdac.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wsbdac.cursomc.domain.Categoria;
+import com.wsbdac.cursomc.domain.Cliente;
+import com.wsbdac.cursomc.repositories.ClienteRepository;
+import com.wsbdac.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(    
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())); 
+	}
+}
