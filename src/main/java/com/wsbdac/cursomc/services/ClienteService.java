0b4fc31f5ef9ba94fa52125f9d1b7ea5dc095726
William Santos <william.santos88@gmail.com>
2019-01-28 01:09:44 -0200

    Atualização de registros com PUT e outros ajustes

--- src/main/java/com/wsbdac/cursomc/services/ClienteService.java
+++ src/main/java/com/wsbdac/cursomc/services/ClienteService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.wsbdac.cursomc.domain.Categoria;
 import com.wsbdac.cursomc.domain.Cliente;
 import com.wsbdac.cursomc.repositories.ClienteRepository;
 import com.wsbdac.cursomc.services.exception.ObjectNotFoundException;
@@ -16,7 +15,7 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
-	public Cliente buscar(Integer id) {
+	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(    
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())); 
