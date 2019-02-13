45067a910195810cbfae989e647d0f7a3a4f0a22
William Santos <william.santos88@gmail.com>
2019-02-13 20:20:01 -0200

    Validação sintática com BeanValidation

--- src/main/java/com/wsbdac/cursomc/services/CategoriaService.java
+++ src/main/java/com/wsbdac/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.wsbdac.cursomc.domain.Categoria;
+import com.wsbdac.cursomc.dto.CategoriaDTO;
 import com.wsbdac.cursomc.repositories.CategoriaRepository;
 import com.wsbdac.cursomc.services.exception.DataIntegrityException;
 import com.wsbdac.cursomc.services.exception.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 		
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
