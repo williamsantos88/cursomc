5bbd8c5dc07e51c1cdcee76752cf907bdef34ff5
William Santos <william.santos88@gmail.com>
2019-02-12 22:01:13 -0200

    Paginação com parâmetros na requisção

--- src/main/java/com/wsbdac/cursomc/services/CategoriaService.java
+++ src/main/java/com/wsbdac/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.wsbdac.cursomc.domain.Categoria;
@@ -45,7 +48,11 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll() {
 		return repo.findAll();
-//		return obj.orElseThrow(() -> new ObjectNotFoundException(    
-//				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+		
 	}
 }
